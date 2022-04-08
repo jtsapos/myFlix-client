b2af728e8d8debfd5359549cc19a4770fcde7fa2
jtsapos <jtsapos@gmx.com>
2022-04-08 19:47:20 -0400

    Created login-view.jsx

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,29 @@
+//useState() creates a local state and preserves it between the render cycles, giving you one of the biggest advantages of declaring a class component without having to actually declare it
+import React, { useState } from 'react';
+
+export function LoginView(props) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username, password);
+        /* Send a request to the server for authentication */
+        /* then call props.onLoggedIn(username) */
+        props.onLoggedIn(username);
+    };
+
+    return (
+        <form>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            </label>
+            <label>
+                Password:
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            </label>
+            <button type="submit" onClick={handleSubmit}>Submit</button>
+        </form>
+    );
+}
