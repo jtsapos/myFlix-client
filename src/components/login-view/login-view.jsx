d5abd9f261403a49b3562c1f79ed3e8d0d1a274e
jtsapos <jtsapos@gmx.com>
2022-04-09 15:18:10 -0400

    Updated login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,12 +1,16 @@
 //useState() creates a local state and preserves it between the render cycles, giving you one of the biggest advantages of declaring a class component without having to actually declare it
 import React, { useState } from 'react';
 
+
+// Create LoginView as function component using Hooks
 export function LoginView(props) {
+    // Call useState method from React to initialize login variables with an empty value
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+    // Sending request to server for authentication
     const handleSubmit = (e) => {
-        e.preventDefault();
+        e.preventDefault();   // prevent default submit button behaviour, i.e., don't reload the page
         console.log(username, password);
         /* Send a request to the server for authentication */
         /* then call props.onLoggedIn(username) */
