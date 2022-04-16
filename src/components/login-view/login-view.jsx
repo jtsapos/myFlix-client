c6e07573fc702ce30510dc82410dce7aa2fd36bb
jtsapos <jtsapos@gmx.com>
2022-04-15 20:29:56 -0400

    Updated login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,8 +1,7 @@
 //useState() creates a local state and preserves it between the render cycles, giving you one of the biggest advantages of declaring a class component without having to actually declare it
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import { Form, Button, } from "react-bootstrap";
 
 
 // Create LoginView as function component using Hooks
@@ -31,9 +30,7 @@ export function LoginView(props) {
                 <Form.Label>Password:</Form.Label>
                 <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
             </Form.Group>
-            <Button variant="primary" type="submit" onClick={handleSubmit}>
-                Submit
-            </Button>
+            <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
         </Form>
     );
 }
