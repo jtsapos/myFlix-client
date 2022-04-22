0bdb39702e13e8d57ba047f9db7357887501a1aa
jtsapos <jtsapos@gmx.com>
2022-04-22 15:45:32 -0400

    Updated src\components\movie-card\movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,47 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import "./movie-card.scss"
-
 import { CardGroup, Container, Button, Card } from "react-bootstrap";
 
+import './movie-card.scss'
+
+
 export class MovieCard extends React.Component {
+
     render() {
         const { movie, onMovieClick } = this.props;
-
         return (
+
             <Container>
                 <CardGroup>
                     <Card id="movie-card">
                         <a><Card.Img variant="top" src={movie.ImagePath} /></a>
                         <Card.Body>
                             <Card.Title id="card-title">{movie.Title}</Card.Title>
-                            {/*<Card.Text>{movie.Description}</Card.Text>*/}
-                            <Button id="card-button" onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+                            <Button id="card-button" onClick={() => onMovieClick(movie)} variant="link">Show more</Button>
                         </Card.Body>
                     </Card>
                 </CardGroup>
             </Container>
         )
-
     };
 }
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
+//export default MovieCard;
+
 /* Use propTypes to validate data types of props
     Validation logic:
     movie object is required, if object contains a title, the Title has to be a string
     onMovieClick function is required
 */
-MovieCard.propTypes = {
-    movie: PropTypes.shape({
-        Title: PropTypes.string.isRequired,
-        //Description: PropTypes.string.isRequired,
-        //Genre: PropTypes.shape({
-        //  Name: PropTypes.string.isRequired
-        //}),
-        //Director: PropTypes.shape({
-        //  Name: PropTypes.string.isRequired
-        //})
-        //Actors: PropTypes.string.isRequired,
-        //ImagePath: PropTypes.string.isRequired
-    }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
-};
+//MovieCard.propTypes = {
+  //  movie: PropTypes.shape({
+  //      Title: PropTypes.string.isRequired,
+  //      Description: PropTypes.string.isRequired,
+  //      Genre: PropTypes.shape({
+  //          Name: PropTypes.string.isRequired
+  //      }),
+  //      Director: PropTypes.shape({
+  //          Name: PropTypes.string.isRequired
+  //      }),
+  //      Actors: PropTypes.string.isRequired,
+  //      ImagePath: PropTypes.shape
+  //  }).isRequired,
+  //  onMovieClick: PropTypes.func.isRequired
+//};
