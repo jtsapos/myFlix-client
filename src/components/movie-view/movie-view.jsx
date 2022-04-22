2784c56bcfda51367c71b238924d428ded8e9480
jtsapos <jtsapos@gmx.com>
2022-04-22 15:45:50 -0400

    Updated src\components\movie-view\movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import propTypes from 'prop-types';
-import "./movie-view.scss"
+import PropTypes from 'prop-types';
+import './movie-view.scss'
 
 import { Card, Col, Container, Row, Button } from "react-bootstrap";
 
@@ -17,13 +17,16 @@ export class MovieView extends React.Component {
                             <Card.Body>
                                 <Card.Img id="movie-view-image" variant="top" src={movie.ImagePath} />
                                 <Card.Title id="movie-title" className="movie-title">{movie.Title}</Card.Title>
-                                <Card.Text id="movie-description" className="movie-description">{movie.Description}</Card.Text>
-                                <Card.Text id="movie-director" className="movie-director">Director: {movie.Director.Name}</Card.Text>
-                                <Card.Text id="movie-genre" className="movie-gerne">Genre: {movie.Genre.Name}</Card.Text>
+                                <Card.Text id="movie-description" className="movie-description">
+                                    {movie.Description}</Card.Text>
+                                <Card.Text id="movie-director" className="movie-director">
+                                    Director: {movie.Director.Name}</Card.Text>
+                                <Card.Text id="movie-genre" className="movie-gerne">
+                                    Genre: {movie.Genre.Name}</Card.Text>
 
                             </Card.Body>
                         </Card>
-                        <Button id="movie-view-button" onClick={() => { onBackClick(null); }}>Back</Button> {/*onClick() event listener sets selectedMovie variable in main-view to null, allowing to return back to list of MovieCards*/}
+                        <Button id="movie-view-button" onClick={() => { onBackClick(null); }}>Back</Button>
                         <Button id="movie-view-button" onClick={() => { }}>Add to favorites</Button>
                     </Col>
                 </Row>
@@ -32,26 +35,29 @@ export class MovieView extends React.Component {
     }
 }
 
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired
+        }),
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequired,
+            Birth: PropTypes.string.isRequired
+        }),
+        Actors: PropTypes.array,
+        ImagePath: PropTypes.string.isRequired
+    }).isRequired
+};
+
+
+//export default MovieView;
+
 /* Use propTypes to validate data types of props
     Validation logic:
     movie object is required, if object contains a title, the Title has to be a string
     onBackClick function is required
 */
-MovieView.propTypes = {
-    movie: propTypes.shape({
-        Title: propTypes.string.isRequired,
-        Description: propTypes.string.isRequired,
-        Genre: propTypes.shape({
-            Name: propTypes.string.isRequired,
-            Description: propTypes.string.isRequired
-        }),
-        Director: propTypes.shape({
-            Name: propTypes.string.isRequired,
-            Bio: propTypes.string.isRequired,
-            Birth: propTypes.string.isRequired,
-        }),
-        Actors: propTypes.array,
-        ImagePath: propTypes.string.isRequired
-    }).isRequired,
-    onBackClick: propTypes.func.isRequired
-};
