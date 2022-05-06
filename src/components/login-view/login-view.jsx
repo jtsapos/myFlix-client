9e89ebd0d9c111c4e72c884450ade7a55f2a2fb6
jtsapos <jtsapos@gmx.com>
2022-05-06 15:09:23 -0400

    Updated src\components\login-view\login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,8 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import { Form, Button, Card, CardGroup, Container, Col, Row } from 'react-bootstrap';
 
 import './login-view.scss';
 
@@ -12,41 +11,83 @@ export function LoginView(props) {
     const [username, setUsername] = useState(''); // Call useState method from React to initialize login variables with an empty value
     const [password, setPassword] = useState('');
 
+    //validation declarations
+    const [usernameErr, setUsernameErr] = useState('');
+    const [passwordErr, setPasswordErr] = useState('');
+
+    const validate = () => {
+        let isReq = true;
+        if (!username) {
+            setUsernameErr('Username Required');
+            isReq = false;
+        } else if (username.length < 2) {
+            setUsernameErr('Username must be at least 2 characters long');
+            isReq = false;
+        }
+        if (!password) {
+            setPasswordErr('Password Required');
+            isReq = false;
+        } else if (password.length < 6) {
+            setPasswordErr('Password must be at least 6 characters long');
+            isReq = false;
+        }
+        return isReq;
+    }
+
     const handleSubmit = (e) => {
-        e.preventDefault();  // prevent default submit button behaviour, i.e., don't reload the page
-
-        /* Send a request to the server for authentication */
-        axios.post('https://myflixs.herokuapp.com/login', {
-
-            Username: username,
-            Password: password
-        })
-            .then(response => {
-                const data = response.data;
-                props.onLoggedIn(data); //When a user logs in, the props onLoggedIn(data) is passed to the LoginView and triggers the function onLoggedIn(authData) in the MainView
+        e.preventDefault();
+        const isReq = validate();
+        if (isReq) {
+            /* Send a request to the server for authentication */
+            axios.post('https://myflixs.herokuapp.com/login', {
+                Username: username,
+                Password: password
             })
-            .catch(e => {
-                console.log('no such user')
-            });
+                .then(response => {
+                    const data = response.data;
+                    props.onLoggedIn(data);
+                })
+                .catch(e => {
+                    console.log('no such user')
+                });
+        }
     };
-
     return (
-        <Form>
-            <Form.Group controlId="formUsername">
-                <Form.Label>Username:</Form.Label>
-                <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
-            </Form.Group>
-            <Form.Group controlId="formPassword">
-                <Form.Label>Password:</Form.Label>
-                <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-            </Form.Group>
+        <Container id="login-form">
+            <Row>
+                <Col>
+                    <CardGroup>
+                        <Card id="login-card">
+                            <Card.Body>
+                                <Card.Title id="login-card-title">Please login</Card.Title>
+                                <Form>
+                                    <Form.Group controlId="formUsername">
+                                        <Form.Label id="login-form-label">Username</Form.Label>
+                                        <Form.Control type="text" onChange={e => setUsername(e.target.value)}
+                                            placeholder="Enter your username" />
+                                        {usernameErr && <p>{usernameErr}</p>}
+                                    </Form.Group>
+                                    <Form.Group controlId="formPassword">
+                                        <Form.Label id="login-form-label">Password</Form.Label>
+                                        <Form.Control type="password" onChange={e => setPassword(e.target.value)}
+                                            placeholder="Enter your password" />
+                                        {passwordErr && <p>{passwordErr}</p>}
+                                    </Form.Group>
+                                    <Button id="login-button" variant="primary" type="submit" onClick={handleSubmit}>Login</Button>
+                                </Form>
+                                <Card.Text>Not registered yet?</Card.Text>
+                                <div id="register-container">
+                                    <Link to="/register">
+                                        <Button id="link-to-register-button">Register now</Button>
+                                    </Link>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </CardGroup>
+                </Col>
+            </Row>
+        </Container>
 
-            <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
-        </Form>
-        //<div><br />
-        //  <span>Need to create an account?</span><br />
-        // <button type="submit">Register Here</button>
-        //</div></>
     );
 }
 
