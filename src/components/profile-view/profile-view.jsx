ab44a9fe77e078389ad753437185b0a694ec7c36
jtsapos <jtsapos@gmx.com>
2022-05-11 16:29:02 -0400

    Updated src\components\profile-view\profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -6,6 +6,7 @@ import PropTypes from "prop-types";
 import { Container, Card, Button, Row, Col, Form, FormGroup, FormControl } from "react-bootstrap";
 import axios from "axios";
 
+
 export class ProfileView extends React.Component {
     constructor() {
         super();
