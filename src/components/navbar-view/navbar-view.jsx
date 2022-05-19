537e607d767b54080012726e86e80d78fb55a0dc
jtsapos <jtsapos@gmx.com>
2022-05-19 14:55:48 -0400

    Updated src\components\navbar-view\navbar-view.jsx

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -1,27 +1,25 @@
 import React from "react";
 import './navbar-view.scss';
 
-import { Navbar, Container, Nav, Button, Offcanvas, NavDropdown, Form, FormControl } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button, Form } from 'react-bootstrap';
 
 
 export function NavbarView({ user }) {
 
-    const onLoggedOut = () => {
-        localStorage.clear();
-        window.open('/', '_self');
-    };
 
     return (
-        <Navbar id="navbar" static="top">
-            <Container id="navbar-container">
-                <Navbar.Brand id="navbar-brand" href="#">myFlix</Navbar.Brand>
+
+        <Container id="navbar-container">
+            <Navbar id="navbar" static="top">
+
+                <Navbar.Brand id="navbar-brand" href="/">myFlix</Navbar.Brand>
                 <Nav id="nav" className="me-auto">
-                    <Nav.Link id="nav-link" href="#home">Account</Nav.Link>
-                    <Nav.Link id="nav-link" href="#features">Watchlist</Nav.Link>
-                    <Nav.Link id="nav-link" href="#register">Register</Nav.Link>
+                    <Nav.Link id="nav-link" href="/profile">Profile</Nav.Link>
+                    <Nav.Link id="nav-link" href="#">Watchlist</Nav.Link>
                 </Nav>
-            </Container>
-        </Navbar>
+            </Navbar>
+        </Container>
+
 
     )
 }
