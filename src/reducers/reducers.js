38782f0f3cf28ce43390d6010d1d6211feb01af2
jtsapos <jtsapos@gmx.com>
2022-06-17 17:35:48 -0400

    Updated src\reducers\reducers.js

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'; //Redux comes with this built-in function to combine reducers
 
-import { SET_FILTER, SET_MOVIES } from '../actions/actions';
+import { SET_FILTER, SET_MOVIES, SET_USER  } from '../actions/actions';
 
 function visibilityFilter(state = '', action) { //visibilityFilter reducer function
   switch (action.type) {
@@ -14,15 +14,27 @@ function visibilityFilter(state = '', action) { //visibilityFilter reducer funct
 function movies(state = [], action) { //movies reducer function
   switch (action.type) {
     case SET_MOVIES:
+        console.log('SET_MOVIES reducer reached');
       return action.value;
     default:
       return state;
   }
 }
 
+function user(state = '', action) { //user reducer function
+    switch (action.type) {
+      case SET_USER:
+          console.log('SET_MOVIES reducer reached');
+        return action.value;
+      default:
+        return state;
+    }
+  }
+
 const moviesApp = combineReducers({ //shortened version of the below combined reducers
     visibilityFilter,
-    movies
+    movies,
+    user
   });
 
 //function moviesApp(state = {}, action) { //So, the moviesApp is a combined reducer (a reducer made out of the two reducers above)
