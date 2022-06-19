7baa886402be60ed9c1bd2b9e83e8b75cd7a5910
jtsapos <jtsapos@gmx.com>
2022-06-19 19:37:05 -0400

    Updated dist\index.b4b6dfad.js

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -25034,7 +25034,7 @@ class MainView extends _reactDefault.default.Component {
             genres: [],
             movies: [],
             FavoriteMovies: [],
-            us: {}
+            usr: {}
         };
     }
     getMovies(token) {
@@ -25062,7 +25062,7 @@ class MainView extends _reactDefault.default.Component {
         //this is updated from login-view (props.onLoggedIn(data))
         //console.log(authData);
         this.setState({
-            us: authData.user,
+            usr: authData.user,
             user: authData.user.Username,
             FavoriteMovies: authData.user.FavoriteMovies
         });
